39a171857c5f07083b56ba1338e61ecf6a7fd616
Emma <you@example.com>
2021-02-10 14:51:48 +0100

    h

--- AppPHP/webconfig.php
+++ AppPHP/webconfig.php
@@ -2,7 +2,7 @@
 //mirar el host es el nombre del contenedro de mysql
 define('DNS','mysql:dbname=Catalogo;host=mysqlc');  
 define('USERNAME','root');  
-define('PASSWORD','toor');
+define('PASSWORD','root');
 ?>
 
 		
